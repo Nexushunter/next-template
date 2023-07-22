04c51372db3464ee287a25fc772ddb45db114b5f
Nexushunter <nexus@nexushunter.dev>
2023-07-22 12:05:20 -0400

    Try adding tracing routes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../")
+  },
+  images: {
+    unoptimized: true,
+  },
+  output: 'standalone'
+}
 
 module.exports = nextConfig
